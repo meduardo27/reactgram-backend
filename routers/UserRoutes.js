8d653b9ed4c400bf902243a0c0c007329805edd5
Mateus Eduardo <medu2710@gmail.com>
2023-03-29 13:52:26 -0300

    feat: manter photo e validacoes

--- routers/UserRoutes.js
+++ routers/UserRoutes.js
@@ -6,6 +6,8 @@ const {
   register,
   login,
   getCurrentUser,
+  update,
+  getUserById,
 } = require("../controllers/UserController");
 
 // Middlewares
@@ -14,10 +16,22 @@ const { authGuard } = require("../middlewares/authGuard");
 const {
   userCreateValidation,
   loginValidation,
+  userUpdateValidation,
 } = require("../middlewares/userValidation");
+const { imageUpload } = require("../middlewares/imageUpload");
 
 // Routes
 router.post("/register", userCreateValidation(), validate, register);
 router.post("/login", loginValidation(), validate, login);
 router.get("/profile", authGuard, getCurrentUser);
+router.put(
+  "/",
+  authGuard,
+  userUpdateValidation(),
+  validate,
+  imageUpload.single("profileImage"),
+  update
+);
+router.get("/:id", getUserById)
+
 module.exports = router;
